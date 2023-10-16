e88de45aba81abe892f84d3ff4a94759d367f95a
DTadpole <gsk390@gmail.com>
2023-10-16 00:56:04 +0000

    scripts

---
+++ download.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_train.tar --no-check-certificate
+wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_val.tar --no-check-certificate
