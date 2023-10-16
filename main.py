646352148ea4ac8519d1d89b77c9bd832e22572d
DTadpole <gsk390@gmail.com>
2023-10-16 05:45:26 +0000

    tune

--- main.py
+++ main.py
@@ -200,7 +200,7 @@ def main():
         train_sampler = torch.utils.data.distributed.DistributedSampler(
             train_dataset)
         val_sampler = torch.utils.data.distributed.DistributedSampler(
-            val_dataset, shuffle=True, drop_last=False)
+            val_dataset, shuffle=True, drop_last=True)
 
         # data loader
         train_loader = torch.utils.data.DataLoader(
